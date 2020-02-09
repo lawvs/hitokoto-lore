a1ec515fc6371e5defda7fdd11cdd53854f3ce8e
lawvs <lawvs@users.noreply.github.com>
2020-02-10 02:45:31 +0900

    feat: add underlords abilities

--- underlords.js
+++ underlords.js
@@ -3,6 +3,7 @@ import fs from 'fs-extra'
 import path from 'path'
 
 const outputDir = path.resolve(global.BUILD_ROOT, './underlords')
+const abilitiesOutputDir = path.resolve(global.BUILD_ROOT, './underlords-abilities')
 
 fs.ensureDirSync(outputDir)
 
@@ -11,13 +12,13 @@ const LANGUAGE_MAPPING = [
     locale: 'zh-CN',
     lng: '简体中文',
     fileKey: 'zh-CN',
-    delimiter: '-',
+    delimiter: /-|—/,
   },
   {
     locale: 'ja-JP',
     lng: '日本語',
     fileKey: 'ja',
-    delimiter: /—|－/,
+    delimiter: /—|－|ー <i>/,
   },
   {
     locale: 'en-US',
@@ -37,7 +38,15 @@ const blockKeys = [
   'dac_item_anessixs_gift_lore',
   'dac_item_cinderwall_lore',
   'dac_map_challenge_combine_3stars_lore',
+  // abilities
+  'dac_ability_anessix_summon_companions_lore',
+  'dac_ability_anessix_summon_golem_lore',
+  'dac_ability_anessix_companion_archer_passive_lore',
+  'dac_ability_anessix_pure_pain_lore',
+  'dac_ability_anessix_martyrs_boon_lore',
+  'dac_ability_demon_warrior_bash_lore',
 ]
+
 /**
  * filter aimless data
  * @param {{ [k: string]: string }} json
@@ -47,10 +56,18 @@ const blockKeys = [
  */
 const filterKey = json =>
   Object.fromEntries(
-    Object.entries(json).filter(
-      ([key, value]) =>
-        key.endsWith('_lore') && !blockKeys.includes(key) && value && !value.endsWith('Lore'),
-    ),
+    Object.entries(json)
+      .map(([key, value]) => [key.trim(), value.trim()])
+      .filter(
+        ([key, value]) =>
+          key.endsWith('_lore') &&
+          !key.endsWith('_description_lore') &&
+          !blockKeys.includes(key) &&
+          value,
+        // !value.endsWith('lore') &&
+        // !value.endsWith('Lore') &&
+        // !value.endsWith('の説明'),
+      ),
   )
 
 /**
@@ -63,8 +80,8 @@ const format = ({ delimiter, data, ...rest }) => ({
     const [content, author] = lore.split(delimiter)
     return {
       key,
-      content,
-      author: author?.trim().replace(/^<i>|<\/i>$/g, ''),
+      content: content.trim().replace(/^<i>|<\/i>$/g, ''),
+      author: author?.trim().replace(/^<i>|^<I>|<\/i>$|<i>$/g, ''),
     }
   }),
 })
@@ -72,8 +89,8 @@ const format = ({ delimiter, data, ...rest }) => ({
 /**
  * @param {{ locale: string, data: Object[]}} json
  */
-const output = json => {
-  const langDir = path.resolve(outputDir, json.locale)
+const output = dir => json => {
+  const langDir = path.resolve(dir, json.locale)
   fs.ensureDirSync(langDir)
   fs.outputJson(path.resolve(langDir, 'all.json'), json.data, { spaces: 2 })
   json.data.forEach((value, index) => {
@@ -89,7 +106,7 @@ const mapData = f => json => ({ ...json, data: f(json.data) })
  */
 const data = files.map(mapData(filterKey)).map(format)
 
-data.map(output)
+data.map(output(outputDir))
 
 const meta = {
   name: 'underlords',
@@ -99,3 +116,25 @@ const meta = {
 }
 
 fs.outputJsonSync(path.resolve(outputDir, 'meta.json'), meta, { spaces: 2 })
+
+// build underlords abilities
+console.log('Build underlords abilities...')
+
+const abilitiesFiles = LANGUAGE_MAPPING.map(({ fileKey, locale, delimiter }) => ({
+  delimiter,
+  locale,
+  data: require(`underlordsconstants/build/underlords_localization_abilities_${fileKey}.json`),
+}))
+
+const abilitiesData = abilitiesFiles.map(mapData(filterKey)).map(format)
+
+abilitiesData.map(output(abilitiesOutputDir))
+
+const abilitiesMeta = {
+  name: 'underlords-abilities',
+  languages: abilitiesData.map(d => d.locale),
+  total: abilitiesData[0].data.length,
+  created: new Date(),
+}
+
+fs.outputJsonSync(path.resolve(abilitiesOutputDir, 'meta.json'), abilitiesMeta, { spaces: 2 })
