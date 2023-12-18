f7609c619e8f62ddbe1affc4430b1b22173bef5c
Whitewater <me@waterwater.moe>
2023-12-18 11:41:52 +0000

    feat: migrate to pnpm with vite and update build process

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    emptyOutDir: false,
+    outDir: 'build',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Lore',
+      fileName: (format) => `main.${format === 'cjs' ? 'js' : format}.js`,
+    },
+  },
+})
